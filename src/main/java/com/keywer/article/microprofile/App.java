8c0cd41c57107ed24b44cf2724f0cf4064d710c0
Sandrine MUTALIPH <baptiste.meynier@gmail.com>
2020-04-04 23:31:35 +0200

    First commit

---
+++ src/main/java/com/keywer/article/microprofile/App.java
@@ -0,0 +1,13 @@
+package com.keywer.article.microprofile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
